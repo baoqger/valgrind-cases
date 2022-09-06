410f46493f5324d5ef6254a9a77de8d2c7c40496
chris bao <baoqger@aliyun.com>
2022-09-07 07:50:08 +0800

    indirectly loss

---
+++ indirectly-lost/main-fix.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct numbers {
+    int n;
+    int *nums;
+};
+
+int n;
+
+struct numbers *numbers;
+
+void create_numbers(struct numbers **nrs, int *n) {
+    *n = 3; // length of outter array
+    *nrs = malloc((sizeof(struct numbers) * 3));  // array of struct numbers
+    struct numbers *nm = *nrs; // why need this? 
+    for (int i = 0; i < 3; i++) {
+        nm->n = i + 1; // length of the inner array
+        nm->nums = malloc(sizeof(int) * (i + 1));
+        for (int j = 0; j < i + 1; j++) {
+            nm->nums[j] = i + j;
+        }
+        nm++;
+    }
+}
+
+void output_report(struct numbers *numbers) {
+    puts("numbers");
+    for (int i = 0; i < numbers->n; i++) {
+        printf("Number: %d\n", numbers->nums[i]);
+    }
+    printf("\n");
+}
+
+void free_memo(struct numbers *numbers) {
+    for (int i = 0; i < 3; i++) {
+        free((numbers+i)->nums);
+    }    
+    free(numbers);
+}
+
+int main() {
+    create_numbers(&numbers, &n);
+    for (int i = 0; i < 2; i++) {
+        output_report(&numbers[i]);
+    }
+    free_memo(numbers);
+    return 0;
+}
